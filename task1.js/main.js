5dc9f6b57cfc0e719f5675d19f656fa2a3f54f61
saba-atia <sabaatia2020@gmail.com>
2024-12-29 22:29:41 +0300

    tasks1.js

---
+++ task1.js/main.js
@@ -0,0 +1,47 @@
+let x;
+x = 1000;
+let y;
+y=500;
+window .document .write("cash flow ratio =")
+window .document .write((x/y)+ "<br>");
+
+window .document .write("net income =")
+window .document .write((x-y)+"<br>");
+
+window .document .write("total assets =")
+window .document .write((x+y)+"<br>");
+
+window .document .write("net income =")
+window .document .write((x*y)+"<br>")
+
+let s;
+s=7;
+let a;
+a=9;
+let b;
+b=2;
+window .document .write("average =")
+window .document .write(((s+a+b)/3)+"<br>");
+let p;
+p=150;
+let d;
+d=0.3;
+window .document .write("discount =")
+window .document .write ((p-(p*d))+"<br>"); 
+
+let age=20;
+window .document .write((age > 18 && age < 30) + "<br>");
+
+let number1=2;
+let number2=3;
+window .document .write(Math.pow(number1,number2)+ "<br>");
+
+let number3=10;
+let number4=4;
+window .document .write((number3 % number4) + "<br>");
+
+
+
+
+
+
